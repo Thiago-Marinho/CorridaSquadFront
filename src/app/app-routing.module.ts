a23b86375539a9ceb8548f0f332267ec518652d7
Thiago-Marinho <peres.marinho@gmail.com>
2021-09-27 17:08:26 -0400

    Criação do Front end de Mecanico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,17 @@ import { CarroComponent } from './components/carro/carro.component';
 import { PilotoAlterarComponent } from './components/piloto-alterar/piloto-alterar.component';
 import { PilotoComponent } from './components/piloto/piloto.component';
 import { ServicoComponent } from './components/servico/servico.component';
+import { MecanicoComponent } from './components/mecanico/mecanico.component';
+import { MecanicoAlterarComponent } from './components/mecanico-alterar/mecanico-alterar.component';
 
 const routes: Routes = [
   { path: "carro", component: CarroComponent},
   { path: "carro/alterar/:id", component: CarroAlterarComponent},
   { path: "piloto", component: PilotoComponent},
   { path: "piloto/alterar/:id", component: PilotoAlterarComponent},
-  { path: "servico", component: ServicoComponent}
-  // { path: "", component: },
-  // { path: "", component: },
+  { path: "servico", component: ServicoComponent},
+  { path: "mecanico", component:MecanicoComponent},
+  { path: "mecanico/alterar/:id", component:MecanicoAlterarComponent}
   // { path: "", component: },
   // { path: "", component: },
   // { path: "", component: },
--- src/app/services/mecanico.service.ts
+++ src/app/services/mecanico.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { mecanico } from '../entities/mecanico';
 @Injectable({
   providedIn: 'root'
 })
 export class MecanicoService {
 
-  constructor() { }
+  url = 'http://localhost:8080/mecanico'
+
+  constructor(private http: HttpClient) { }
+
+
+  loadMecanico(id: number): Observable<mecanico>
+  {
+    const getUrl = `${this.url}/${id}`
+    return this.http.get<mecanico>(getUrl)
+  }
+
+  listar()
+  {
+    return this.http.get<mecanico[]>(`${this.url}/listar`)
+  }
+
+  incluir(novoMecanico: mecanico): Observable<mecanico>
+  {
+    return this.http.post<mecanico>(`${this.url}/incluir`, novoMecanico)
+  }
+
+  alterar(mecanico: mecanico): Observable<any>
+  {
+    return this.http.put<any>(`${this.url}/alterar`, mecanico)
+  }
 }
