e032c58539ce051a5ad2f803090baa3d628e6009
emersonnobre <egrnobre@gmail.com>
2021-09-27 10:59:46 -0400

    criação, configuração e criação de um modelo de funcionamento para o projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarroAlterarComponent } from './components/carro-alterar/carro-alterar.component';
+import { CarroComponent } from './components/carro/carro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "carro", component: CarroComponent},
+  { path: "carro/alterar/:id", component: CarroAlterarComponent}
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: },
+  // { path: "", component: }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarroComponent } from './components/carro/carro.component';
+import { CarroAlterarComponent } from './components/carro-alterar/carro-alterar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarroComponent,
+    CarroAlterarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
